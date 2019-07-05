8eb6a227b622e674e9ff8449a7ab589856c70fc9
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2019-07-04 22:07:17 -0500

    Add an -O3 optimized asm iterative procedure

--- fib_clib/__init__.py
+++ fib_clib/__init__.py
@@ -1,6 +1,6 @@
 from . import imp
 
-__all__ = [ "recursive_dynamic", "iterative", "power_matrix", "iterative_asm", "recursive_dynamic_O3", "iterative_O3", "power_matrix_O3", "algos"]
+__all__ = [ "recursive_dynamic", "iterative", "power_matrix", "iterative_asm", "recursive_dynamic_O3", "iterative_O3", "iterative_asm_03", "power_matrix_O3", "algos"]
 
 recursive_dynamic = imp.recursive_dynamic
 iterative = imp.iterative
@@ -9,6 +9,7 @@ power_matrix = imp.power_matrix
 
 recursive_dynamic_O3 = imp.recursive_dynamic_O3
 iterative_O3 = imp.iterative_O3
+iterative_asm_O3 = imp.iterative_asm_O3
 power_matrix_O3 = imp.power_matrix_O3
 
 algos = {'recursive_dynamic': recursive_dynamic,
@@ -17,4 +18,5 @@ algos = {'recursive_dynamic': recursive_dynamic,
          'power_matrix': power_matrix,
          'recursive_dynamic_O3': recursive_dynamic_O3,
          'iterative_O3': iterative_O3,
+         'iterative_asm_O3': iterative_asm_O3,
          'power_matrix_O3': power_matrix_O3}
--- fib_clib/imp.py
+++ fib_clib/imp.py
@@ -15,6 +15,8 @@ libfib_clibO3.recursive_dynamic.restype = ctypes.c_longlong
 libfib_clibO3.recursive_dynamic.argtypes = [ctypes.c_longlong]
 libfib_clibO3.iterative.restype = ctypes.c_longlong
 libfib_clibO3.iterative.argtypes = [ctypes.c_longlong]
+libfib_clibO3.iterative_asm.restype = ctypes.c_longlong
+libfib_clibO3.iterative_asm.argtypes = [ctypes.c_longlong]
 libfib_clibO3.power_matrix.restype = ctypes.c_longlong
 libfib_clibO3.power_matrix.argtypes = [ctypes.c_longlong]
 
@@ -36,6 +38,9 @@ def recursive_dynamic_O3(n):
 
 def iterative_O3(n):
     return int(libfib_clibO3.iterative(n))
+
+def iterative_asm_O3(n):
+    return int(libfib_clibO3.iterative_asm(n))
 
 def power_matrix_O3(n):
     return int(libfib_clibO3.power_matrix(n))
